94d24a3959ecb05b125a2de2e2f54854ee127182
Shannon <shannon.yoshikawa@gmail.com>
2019-05-08 13:35:54 -0700

    completed MVP

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,47 +1,47 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+  Object oriented design is commonly used in video games.  For this part of the assignment you will be 
+  implementing several constructor functions with their correct inheritance hierarchy.
 
   In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
 
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at 
+  the bottom of the page to test your constructor functions.
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
-  
-/*
-  === GameObject ===
-  * createdAt
-  * name
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
-*/
+  //Game Object
+function GameObject(attributes){
+  this.name=attributes.name;
+  this.createdAt= attributes.createdAt;
+  this.dimensions=attributes.dimensions;
+}
+GameObject.prototype.destroy = function(){
+  return (`${this.name} was removed from the game.`)
+}
 
-/*
-  === CharacterStats ===
-  * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
-*/
+//Character Stats
+function CharacterStats(charAttributes){
+  GameObject.call(this,charAttributes);
+  this.healthPoints=charAttributes.healthPoints;
+}
+CharacterStats.prototype = Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage = function(){
+  return (`${this.name} took damage.`)
+}
 
-/*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
-  * team
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
-  * should inherit takeDamage() from CharacterStats
-*/
- 
-/*
-  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
-*/
+//Humanoid 
+ function Humanoid(humanoidAttributes){
+   CharacterStats.call(this,humanoidAttributes);
+   this.team=humanoidAttributes.team;
+   this.weapons=humanoidAttributes.weapons;
+   this.language=humanoidAttributes.language;
+ }
 
-// Test you work by un-commenting these 3 objects and the list of console logs below:
+Humanoid.prototype=Object.create(CharacterStats.prototype);
+Humanoid.prototype.greet = function(){
+  return `${this.name} offers a greeting in ${this.language}`
+}
 
-/*
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +102,43 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+/*
+  === GameObject ===
+
+  * createdAt
+  * name
+  * dimensions (These represent the character's size in the video game)
+  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+
+  // GameObject.prototype.test = function(){
+//   console.log(`Name: ${this.name}, created at ${this.createdAt}, dimensions ${this.dimensions}.`)
+// }
+// const testObj = new GameObject({name:'testName',createdAt:'creationDate', dimensions:'Large'})
+// console.log(testObj.destroy());
+/*
+  === CharacterStats ===
+  * healthPoints
+  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+  * should inherit destroy() from GameObject's prototype
+*/
+/*
+  === Humanoid (Having an appearance or character resembling that of a human.) ===
+  * team
+  * weapons
+  * language
+  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+  * should inherit destroy() from GameObject through CharacterStats
+  * should inherit takeDamage() from CharacterStats
+*/
+/*
+  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+  * Instances of CharacterStats should have all of the same properties as GameObject.
+*/
+
+// Test you work by un-commenting these 3 objects and the list of console logs below:
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,46 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global/Window--this refers to the entire wondow/console as the object, depending on where it is called. If called in a browser, this refers to the browser.
+* 2. Implicit--this refers to whatever object is to the left of the dot. 
+* 3. New --this refers to the object made and returned by using a constructor function
+* 4. Explicit--when there is no object left of a period for this to refer to, you can explicitly state what this refers to by using apply, call, etc.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+function globalThis(name){
+    console.log(this);
+    return name;
+}
+globalThis('me');
 
 // Principle 2
 
-// code example for Implicit Binding
+const yourObj = {
+    greeting: 'Hello',
+    sayHello:function(name){
+        console.log(`${this.greeting} your name is ${name}.`)
+    }
+}
+yourObj.sayHello('Maks')
 
 // Principle 3
+function Person(name){
+    this.name = name;
+    this.greeting = '::approving nod::';
+    this.speak = function(){
+        console.log(this.greeting + this.name);
+    }
+}
 
-// code example for New Binding
+const shannon = new Person('Shannon');
+const cat = new Person('Chalky')
+shannon.speak();
+cat.speak();
 
 // Principle 4
-
-// code example for Explicit Binding
+shannon.speak.apply(cat);
+cat.speak.call(shannon);
